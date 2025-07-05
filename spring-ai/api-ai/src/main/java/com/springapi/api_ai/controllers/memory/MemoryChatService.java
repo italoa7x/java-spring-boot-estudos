8a7ba3c41fe608d6702ec8c9e3824aa380364ff2
Italo <contato.italo1020@gmail.com>
2025-07-05 17:25:09 -0300

    feat: adiciona integração com banco de dados

--- spring-ai/api-ai/src/main/java/com/springapi/api_ai/controllers/memory/MemoryChatService.java
+++ spring-ai/api-ai/src/main/java/com/springapi/api_ai/controllers/memory/MemoryChatService.java
@@ -3,6 +3,8 @@ package com.springapi.api_ai.controllers.memory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +12,12 @@ public class MemoryChatService {
 
     private ChatClient chatClient;
 
-    public MemoryChatService(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public MemoryChatService(ChatClient.Builder chatClientBuilder, JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository) // Essa linha deixa implicito o tipo do jdbc que esta sendo utilizado.
+                .maxMessages(10)
+                .build();
+
         this.chatClient = chatClientBuilder.defaultAdvisors(
             MessageChatMemoryAdvisor.builder(chatMemory).build()
         ).build();
@@ -18,6 +25,7 @@ public class MemoryChatService {
 
     public String simpleChat(String message) {
         return this.chatClient.prompt()
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, "1234"))
                 .user(message)
                 .call()
                 .content();
